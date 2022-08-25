f0966a8f1c740cd5346a7c66efb84a28fa421f75
TurtleEngr <turtle.engr@gmail.com>
2022-08-24 19:40:04 -0700

    Added intitial files.

---
+++ sample-tdd/test-functions.js
@@ -0,0 +1,23 @@
+// Positive tests
+QUnit.module('fAdd OK');
+
+QUnit.test('Test with valid numbers, ver1', assert => {
+  assert.equal(fAdd(1, 2), 3);
+  assert.equal(fAdd(-2, 2), 0);
+});
+
+QUnit.test('Test with numbers as strings, ver2', assert => {
+  assert.equal(fAdd("1", 2), 3);
+  assert.equal(fAdd("1", "2"), 3);
+  assert.equal(fAdd("1.5", 2.5), 4);
+});
+
+// Negative tests
+QUnit.module('fAdd NotOK');
+
+QUnit.test("Test with non-number arguments, ver3", assert => {
+  assert.throws(function () {fAdd("foo",2);}, /First/);
+  assert.throws(function () {fAdd(3,"foo");}, /Second/);
+  assert.throws(function () {fAdd("2foo",2);}, /First/);
+  assert.throws(function () {fAdd(3.5,"2.x");}, /Second/);
+});
